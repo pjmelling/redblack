ceddfc194dd8443b93900c9653db2636224e94dd
Peter Melling <pjmelling@gmail.com>
2016-02-13 17:57:56 -0600

    Add Tree and Node classes

---
+++ lib/redblack/node.rb
@@ -0,0 +1,5 @@
+module RedBlack 
+  class Node
+    attr_accessor :color, :key, :left, :right, :p
+  end
+end
---
+++ lib/redblack/tree.rb
@@ -0,0 +1,42 @@
+module RedBlack
+  class Tree
+    def left_rotate
+    end
+
+    def right_rotate
+    end
+
+    def insert
+    end
+
+    def insert_fixup
+    end
+
+    def delete
+    end
+
+    def delete_fixup
+    end
+
+    def delete
+    end
+
+    def search
+    end
+
+    def predecessor
+    end
+
+    def successor
+    end
+
+    def minimum
+    end
+
+    def maximum
+    end
+
+    def inorder_tree_walk
+    end
+  end
+end
